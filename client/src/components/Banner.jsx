fe04c6d1d8135629c4ee32a53d050dfddb899d3a
dlooz8 <mazurzoolda@gmail.com>
2024-02-08 18:04:41 +0300

    update Banner

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -4,21 +4,21 @@ function Banner() {
     <section className='mx-48 my-12 banner'>
         <div className="banner-1 relative">
             <img className="w-[360px] h-[452px] object-cover rounded-xl" src="banner-car.jpg" alt="img" />
-            <div className="absolute top-[71%] bg-gray m-2.5 p-2.5 rounded-xl max-w-[339px] max-h-[117px]">
+            <div className="glass absolute top-[71%] bg-gray m-2.5 p-2.5 rounded-xl max-w-[339px] max-h-[117px]">
                 <h3>How to Drive a Car Safely</h3>
                 <p className="pt-3 line-clamp-2">Ah, the joy of the open road—it’s a good feeling. But if you’re new to driving, you may feel a little nervous about getting behind the wheel. Don’t worry. While it’s true that accidents can happen to anybody, there are things you can do to drive safely and do your best to avoid them. </p>
             </div>
         </div>
         <div className="banner-2 relative">
-            <img className="w-[360px] h-[452px] object-cover rounded-xl" src="banner-car.jpg" alt="img" />
-            <div className="absolute top-[71%] bg-gray m-2.5 p-2.5 rounded-xl max-w-[339px] max-h-[117px]">
+            <img className="w-[360px] h-[452px] object-cover rounded-xl" src="banner-music.jpg" alt="img" />
+            <div className="glass absolute top-[71%] bg-gray m-2.5 p-2.5 rounded-xl max-w-[339px] max-h-[117px]">
                 <h3>How to Drive a Car Safely</h3>
                 <p className="pt-3 line-clamp-2">Ah, the joy of the open road—it’s a good feeling. But if you’re new to driving, you may feel a little nervous about getting behind the wheel. Don’t worry. While it’s true that accidents can happen to anybody, there are things you can do to drive safely and do your best to avoid them. </p>
             </div>
         </div>
         <div className="banner-3 relative">
-            <img className="w-[360px] h-[452px] object-cover rounded-xl" src="banner-car.jpg" alt="img" />
-            <div className="absolute top-[71%] bg-gray m-2.5 p-2.5 rounded-xl max-w-[339px] max-h-[117px]">
+            <img className="w-[744px] h-[452px] object-cover rounded-xl" src="banner-tech.jpg" alt="img" />
+            <div className="glass absolute top-[71%] bg-gray m-2.5 p-2.5 rounded-xl max-w-[724px] max-h-[117px]">
                 <h3>How to Drive a Car Safely</h3>
                 <p className="pt-3 line-clamp-2">Ah, the joy of the open road—it’s a good feeling. But if you’re new to driving, you may feel a little nervous about getting behind the wheel. Don’t worry. While it’s true that accidents can happen to anybody, there are things you can do to drive safely and do your best to avoid them. </p>
             </div>
