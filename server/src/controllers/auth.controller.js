b09ad566495a2ef069286f00af0e083545b2f422
dlooz8 <mazurzoolda@gmail.com>
2024-02-17 18:53:08 +0300

    fixed hashing processing

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -4,21 +4,25 @@ const bcrypt = require('bcrypt');
 const authController = {
   register: async (req, res) => {
     const { name, email, password, avatar_url } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
     try {
-      const newUser = await prisma.users.create({
-        data: {
-          name: name,
-          email: email,
-          password: hashedPassword,
-          avatar_url: avatar_url,
-        },
-      });
-      req.session.userId = newUser.id;
-      const { password: excludedPassword, ...userWithoutPassword } = newUser;
-      res.status(200).json(userWithoutPassword);
+      const hashedPassword = await bcrypt.hash(password, 10);
+      try {
+        const newUser = await prisma.users.create({
+          data: {
+            name: name,
+            email: email,
+            password: hashedPassword,
+            avatar_url: avatar_url,
+          },
+        });
+        req.session.userId = newUser.id;
+        const { password: excludedPassword, ...userWithoutPassword } = newUser;
+        res.status(200).json(userWithoutPassword);
+      } catch (error) {
+        res.status(500).json({ message: 'Ошибка при создании пользователя' });
+      }
     } catch (error) {
-      res.status(500).json({ message: 'Ошибка при создании пользователя' });
+      res.status(500).json({ message: 'Ошибка при хешировании пароля' });
     }
   },
   login: async (req, res) => {
@@ -27,13 +31,17 @@ const authController = {
     if (!existingUser) {
       res.status(401).json({ message: 'Неверное имя пользователя или пароль' });
     } else {
-      const passwordValid = await bcrypt.compare(password, existingUser.password);
-      if (passwordValid) {
-        req.session.userId = existingUser.id;
-        const { password: excludedPassword, ...userWithoutPassword } = existingUser;
-        res.status(200).json(userWithoutPassword);
-      } else {
-        res.status(401).json({ message: 'Неверное имя пользователя или пароль' });
+      try {
+        const passwordValid = await bcrypt.compare(password, existingUser.password);
+        if (passwordValid) {
+          req.session.userId = existingUser.id;
+          const { password: excludedPassword, ...userWithoutPassword } = existingUser;
+          res.status(200).json(userWithoutPassword);
+        } else {
+          res.status(401).json({ message: 'Неверное имя пользователя или пароль' });
+        }
+      } catch (error) {
+        res.status(401).json({ message: 'Ошибка хеширования пароля' });
       }
     }
   },
